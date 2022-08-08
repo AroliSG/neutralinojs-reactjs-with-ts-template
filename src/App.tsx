06b1209692a7d7de93bd8f64301811f127c519b8
AroliSG <58828449+AroliSG@users.noreply.github.com>
2022-08-08 16:54:22 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App (): JSX.Element {
     <div className="App">
       <nav>
         <Link className='Label' to={'/Home'}>Home</Link>
-        <Link className='Label' to={'/Sample'}>Sample</Link>
+        <Link className='Label' to={'/Sample'}>Test</Link>
       </nav>
       <Outlet/>
     </div>
